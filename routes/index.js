d948359e12770ecd3aab013aea7e7d75b5391336
Jacob <jakeincanada@icloud.com>
2018-10-25 21:01:44 -0400

    remove redundant comments

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,14 @@ const express = require('express');
 const router = express.Router();
 const WordStream = require("../WordStream");
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   (new WordStream({word: "yes"})).pipe(res);
 });
 
-/* GET home page. */
 router.get('/yes', function(req, res, next) {
   (new WordStream({word: "yes"})).pipe(res);
 });
 
-/* GET home page. */
 router.get('/no', function(req, res, next) {
   (new WordStream({word: "no"})).pipe(res);
 });
